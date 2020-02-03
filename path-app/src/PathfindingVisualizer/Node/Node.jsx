6e3647832803d9911b81c51784dc0bf99e991aa9
Brandon Chasser <brandonchasser22@gmail.com>
2020-02-03 12:15:22 -0500

    mouse events

--- path-app/src/PathfindingVisualizer/Node/Node.jsx
+++ path-app/src/PathfindingVisualizer/Node/Node.jsx
@@ -7,14 +7,23 @@ export default class Node extends Component{
 
     render(){
         const{
-            columns,Finish,Start,Wall,rows,
+            columns,Finish,Start,Wall,onMouseDown,onMouseEnter,onMouseUp,rows,
         } = this.props;
-        const extraClassName = Finish ? 'node-finish': Start ? 'node-start' : Wall ? 'node-Wall' : '';
-        return(
+        const extraClassName = Finish 
+        ? 'node-finish'
+        : Start
+        ? 'node-start'
+        : Wall
+        ? 'node-Wall'
+        : '';
+        return (
             <div
             id={`node-${rows}-${columns}`}
             className={`node ${extraClassName}`}
+            onMouseDown={() => onMouseDown(rows,columns)}
+            onMouseEnter={() => onMouseEnter(rows,columns)}
+            onMouseUp={() => onMouseUp()}
             ></div>
-        )
+        );
     }
 }
